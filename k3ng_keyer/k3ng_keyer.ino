b50a76d6512cee2aa2d4f1ba1dcdf0f8de2148c8
robaol <robaol@users.noreply.github.com>
2019-12-20 12:34:51 +0000

    Increased default info display time to 2s

--- k3ng_keyer/k3ng_keyer.ino
+++ k3ng_keyer/k3ng_keyer.ino
@@ -1492,7 +1492,7 @@ unsigned long last_config_write = 0;
 
 #ifdef FEATURE_DISPLAY
   enum lcd_statuses {LCD_CLEAR, LCD_REVERT, LCD_TIMED_MESSAGE, LCD_SCROLL_MSG};
-  #define default_display_msg_delay 1000
+  #define default_display_msg_delay 2000
 #endif //FEATURE_DISPLAY
 
 #ifdef FEATURE_LCD_ADAFRUIT_I2C
